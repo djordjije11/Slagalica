8e6993ff337b4637a47c425032d4ff01e7a9ed70
DESKTOP-A2R6AE6\Djordjije <djordjijeradovic24@gmail.com>
2022-08-20 21:30:27 +0200

    nothing special;

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,12 +1,8 @@
 package test;
 
-import guiClasses.Username;
-
 public class Test {
 
 	public static void main(String[] args) {
-		//new Username();
-		//new Username("Uneti username je vec koriscen. Pokusaj opet!");
+		
 	}
-
 }
